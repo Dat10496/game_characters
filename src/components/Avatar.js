5d08d8babd0779d614efcbb1a1a9bb4ce4e1389d
votada1004 <votada1996@gmal.com>
2022-04-24 16:58:56 +0700

    second-commit

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -9,7 +9,7 @@ function Avatar({ parts }) {
       <BodyPart path="hair" index={parts.hair} zIndex={6} />
       <BodyPart path="mouths" index={parts.mouth} zIndex={4} />
       <BodyPart path="eyebrows" index={parts.eyebrows} zIndex={4} />
-      <BodyPart path="glasses" index={parts.glasses} zIndex={5} />
+      <BodyPart path="accessories/glasses" index={parts.glasses} zIndex={5} />
       <BodyPart path="facial_hair" index={parts.facial_hair} zIndex={4} />
       <BodyPart path="clothes/layer_1" index={parts.clothing1} zIndex={2} />
       <BodyPart path="clothes/layer_2" index={parts.clothing2} zIndex={3} />
